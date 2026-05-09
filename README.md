c1faa08d45322468386bf3b9ad08226e0e2d8e84
Tushar Raj <tr6056538@gmail.com>
2026-05-09 17:23:30 +0530

    created a simplistic UI screen

--- README.md
+++ README.md
@@ -1,56 +1,36 @@
-# Welcome to your Expo app 👋
+# React Native Sign In Screen
 
-This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
+A simple and clean sign-in screen built with React Native and Expo.
 
-## Get started
+## Features
 
-1. Install dependencies
+- Email and password login form
+- Social login options (Facebook, Google, Instagram)
+- Light and dark mode support
+- Sign up and password recovery links
+- Responsive design
 
-   ```bash
-   npm install
-   ```
+## Preview
 
-2. Start the app
+![Sign In Screen](./page_image.jpg)
 
-   ```bash
-   npx expo start
-   ```
+## Getting Started
 
-In the output, you'll find options to open the app in a
-
-- [development build](https://docs.expo.dev/develop/development-builds/introduction/)
-- [Android emulator](https://docs.expo.dev/workflow/android-studio-emulator/)
-- [iOS simulator](https://docs.expo.dev/workflow/ios-simulator/)
-- [Expo Go](https://expo.dev/go), a limited sandbox for trying out app development with Expo
-
-You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
-
-## Get a fresh project
-
-When you're ready, run:
+### Install dependencies
 
 ```bash
-npm run reset-project
+npm install
 ```
 
-This command will move the starter code to the **app-example** directory and create a blank **app** directory where you can start developing.
+### Start the app
 
-### Other setup steps
+```bash
+npx expo start
+```
 
-- To set up ESLint for linting, run `npx expo lint`, or follow our guide on ["Using ESLint and Prettier"](https://docs.expo.dev/guides/using-eslint/)
-- If you'd like to set up unit testing, follow our guide on ["Unit Testing with Jest"](https://docs.expo.dev/develop/unit-testing/)
-- Learn more about the TypeScript setup in this template in our guide on ["Using TypeScript"](https://docs.expo.dev/guides/typescript/)
+Then press:
 
-## Learn more
-
-To learn more about developing your project with Expo, look at the following resources:
-
-- [Expo documentation](https://docs.expo.dev/): Learn fundamentals, or go into advanced topics with our [guides](https://docs.expo.dev/guides).
-- [Learn Expo tutorial](https://docs.expo.dev/tutorial/introduction/): Follow a step-by-step tutorial where you'll create a project that runs on Android, iOS, and the web.
-
-## Join the community
-
-Join our community of developers creating universal apps.
-
-- [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
-- [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+- **a** for Android emulator
+- **i** for iOS simulator
+- **w** for web browser
+- **j** to open Expo Go on your phone
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,176 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+    <SafeAreaView style={styles.container}>
+      {/*for top image*/}
+      <View>
+        <Image
+          source={require("@/assets/images/logo.png")}
+          style={{
+            height: 100,
+            width: 100,
+          }}
+        />
+      </View>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+      {/*for Sign In text*/}
+      <View style={{ alignItems: "center", margin: 10 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 30 }}>Sign In</Text>
+        <Text style={{ color: "#686767" }}>
+          {" "}
+          Let's experience the joy of telecare AI.
+        </Text>
+      </View>
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
+      {/*Sign-in form */}
+      <View style={{ width: "90%", marginVertical: 10 }}>
+        <Text style={styles.label}>Email Address</Text>
+        <View style={styles.inputContainer}>
+          <FontAwesome name="envelope-o" size={14} color="black" />
+          <TextInput placeholder="Enter your email..." style={styles.input} />
+        </View>
+      </View>
+
+      <View style={{ width: "90%", marginVertical: 10 }}>
+        <Text style={styles.label}>Password</Text>
+        <View style={styles.inputContainer}>
+          <AntDesign name="lock" size={14} color="black" />
+          <TextInput
+            placeholder="Enter your password..."
+            secureTextEntry
+            style={styles.input}
           />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+        </View>
+      </View>
 
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+      <View style={{ width: "90%", marginVertical: 15 }}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Sign In</Text>
+          <AntDesign name="arrow-right" size={11} color="white" />
+        </Pressable>
+      </View>
+
+      {/*SSO Login icons */}
+      <View style={styles.ssoContainer}>
+        <Pressable style={styles.ssoButton}>
+          <FontAwesome name="facebook" size={24} color="#000000" />
+        </Pressable>
+        <Pressable style={styles.ssoButton}>
+          <FontAwesome name="google" size={24} color="#000000" />
+        </Pressable>
+        <Pressable style={styles.ssoButton}>
+          <FontAwesome name="instagram" size={24} color="#000000" />
+        </Pressable>
+      </View>
+
+      {/* footer*/}
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Don't have an account? <Text style={styles.footerLink}>Sign Up</Text>
+        </Text>
+        <Text style={[styles.footerLink, { textDecorationLine: "underline" }]}>
+          Forgot your password?
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f6f6f6",
   },
-  safeArea: {
+
+  label: {
+    fontSize: 14,
+    fontWeight: "600",
+    marginBottom: 8,
+    color: "#000",
+  },
+
+  inputContainer: {
+    margin: 0,
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 12,
+    backgroundColor: "#ffffff",
+    borderColor: "transparent",
+    flexDirection: "row",
+    paddingLeft: 10,
+    alignItems: "center",
+  },
+
+  input: {
     flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    fontSize: 14,
   },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
+
+  button: {
+    borderWidth: 1,
+    borderRadius: 12,
+    backgroundColor: "#85cc17",
+    borderColor: "transparent",
+    paddingVertical: 14,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 10,
   },
-  title: {
-    textAlign: 'center',
+
+  buttonText: {
+    color: "#ffffff",
+    fontWeight: "600",
+    fontSize: 16,
   },
-  code: {
-    textTransform: 'uppercase',
+
+  ssoContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    marginVertical: 20,
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+
+  ssoButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 15,
+    borderWidth: 1.5,
+    borderColor: "#e0e0e0",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+
+  footer: {
+    alignItems: "center",
+    marginTop: 15,
+    gap: 8,
+  },
+
+  footerText: {
+    color: "#686767",
+    fontSize: 14,
+  },
+
+  footerLink: {
+    color: "#85cc17",
+    fontWeight: "600",
+    fontSize: 14,
   },
 });
